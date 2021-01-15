5509def0b9e1e60d7a412b8e9a592c7e69c98da6
Rebecca Smith <beccamichelle@live.com>
2021-01-14 20:27:03 -0600

    Add continuous movement

--- snake.js
+++ snake.js
@@ -4,6 +4,8 @@ const ctx = canvas.getContext('2d');
 let x = 0;
 let y = 0;
 
+let lastPress = "ArrowRight";
+
 ctx.fillStyle = 'green';
 ctx.fillRect(x, y, 20, 20);
 
@@ -11,7 +13,18 @@ const log = document.getElementById("log");
 
 document.addEventListener('keydown', logKey);
 
+setInterval(redraw, 500);
+
 function redraw() {
+    if (lastPress === "ArrowUp") {
+        y -= 20;
+    } else if (lastPress === "ArrowDown") {
+        y += 20;
+    } else if (lastPress === "ArrowLeft") {
+        x -= 20;
+    } else if (lastPress === "ArrowRight") {
+        x += 20;
+    }
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, 600, 600);
     ctx.fillStyle = 'green';
@@ -20,16 +33,5 @@ function redraw() {
 
 
 function logKey(e) {
-
-    if (e.code === "ArrowUp") {
-        y -= 20;
-    } else if (e.code === "ArrowDown") {
-        y += 20;
-    } else if (e.code === "ArrowLeft") {
-        x -= 20;
-    } else if (e.code === "ArrowRight") {
-        x += 20;
-    }
-
-    redraw();
+    lastPress = e.code;
 }
