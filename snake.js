41a765cb69844367a021793e7d80f8202d172e02
Rebecca Smith <beccamichelle@live.com>
2021-01-16 14:07:40 -0600

    Add gameover when hitting screen edges and score speeds up the snake

--- snake.js
+++ snake.js
@@ -13,7 +13,7 @@ class Coord {
 }
 
 // Config
-let size = 30
+let size = 64
 let squares = 15
 canvas.height = size * squares;
 canvas.width = size * squares;
@@ -26,38 +26,14 @@ let appleX;
 let appleY;
 let lastPress;
 let gameover = false;
+let speed;
 
 document.addEventListener('keydown', handleKey);
 
 function step() {
     // update snake position
-
-
-
-    /*
-    let newSegmentX = snakeParts[0].x;
-    let newSegmentY = snakeParts[0].y;
-
-    for (let i = 0; i < snakeParts.length - 1; i++) {
-        snakeParts[i].x = snakeParts[i + 1].x;
-        snakeParts[i].y = snakeParts[i + 1].y;
-    }
-
-
-    let head = snakeParts[snakeParts.length - 1];
-
-    if (lastPress === "ArrowUp") {
-        head.y -= size;
-    } else if (lastPress === "ArrowDown") {
-        head.y += size;
-    } else if (lastPress === "ArrowLeft") {
-        head.x -= size;
-    } else if (lastPress === "ArrowRight") {
-        head.x += size;
-    }
-    */
-
     let head = snakeParts[snakeParts.length - 1].copy();
+
     if (lastPress === "ArrowUp") {
         head.y -= size;
     } else if (lastPress === "ArrowDown") {
@@ -69,58 +45,69 @@ function step() {
     }
     snakeParts.push(head);
 
+    //Determines if the snake has hit its tail
     for (let i = 0; i < snakeParts.length - 1; i++) {
         if (snakeParts[i].x === head.x && snakeParts[i].y === head.y) {
-            gameover = true;
-            clearInterval(timer);
-            ctx.fillStyle = 'black';
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-            ctx.fillStyle = 'white';
-            ctx.font = '50px serif';
-            ctx.textAlign = 'center';
-            ctx.fillText("GAME OVER", canvas.width / 2, canvas.height / 2);
-            ctx.font = '30px serif';
-            ctx.textAlign = 'center';
-            ctx.fillText("Press ENTER to start a new game", canvas.width / 2, (canvas.height / 2) + 50);
+            gameOver();
             return;
-
         }
     }
 
+    //Determines if the snake has moved offscreen 
+    if (head.x === canvas.width || head.x < 0 || head.y === canvas.height || head.y < 0) {
+        gameOver();
+        return;
+    }
+
+    //Determines if the snake hits the apple
     if (head.x === appleX && head.y === appleY) {
+
         console.log("gobble!");
         [appleX, appleY] = randomCoord();
         score += 100;
+        speed = speed * 0.95;
+        clearInterval(timer);
+        timer = setInterval(step, speed);
+
     } else {
         // remove tip of tail
         snakeParts.shift();
     }
 
-
-    // handle apple gobbling and update apple position
-    /*
-    if (head.x === appleX && head.y === appleY) {
-        console.log("gobble!");
-        [appleX, appleY] = randomCoord();
-        snakeParts.unshift(new Coord(newSegmentX, newSegmentY));
-        score += 100;
-    }
-    */
-
     redraw();
 }
 
-
-
-function redraw() {
+function gameOver() {
+    gameover = true;
+    clearInterval(timer);
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'white';
+    ctx.font = '50px serif';
+    ctx.textAlign = 'center';
+    ctx.fillText("GAME OVER", canvas.width / 2, canvas.height / 2);
+    ctx.font = '30px serif';
+    ctx.textAlign = 'center';
+    ctx.fillText("Press ENTER to start a new game", canvas.width / 2, (canvas.height / 2) + 50);
+    return;
+}
+
+function redraw() {
+    // clear canvas
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // draw apple
     ctx.fillStyle = 'red';
     ctx.fillRect(appleX, appleY, size, size);
+
+    // draw snake
     snakeParts.forEach(function (segment) {
         ctx.fillStyle = 'green';
         ctx.fillRect(segment.x, segment.y, size, size);
     });
+
+    // draw score
     ctx.fillStyle = 'white';
     ctx.font = '30px serif';
     ctx.textAlign = 'center';
@@ -146,9 +133,12 @@ function randomCoord() {
 }
 
 function restartGame() {
-    snakeParts = [new Coord(0, 0), new Coord(20, 0), new Coord(40, 0), new Coord(60, 0)];
+
+    speed = 250;
+
+    snakeParts = [new Coord(size * 0, 0), new Coord(size * 1, 0), new Coord(size * 2, 0), new Coord(size * 3, 0)];
     lastPress = "ArrowRight";
-    timer = setInterval(step, 250);
+    timer = setInterval(step, speed);
     score = 0;
     [appleX, appleY] = randomCoord();
     gameover = false;
