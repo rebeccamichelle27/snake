0158c12165194e5df27495aae9df4cc26372d898
Rebecca Smith <beccamichelle@live.com>
2021-01-15 19:32:49 -0600

    Dynamic game size and snake growth

--- snake.js
+++ snake.js
@@ -1,37 +1,65 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-let x = 0;
-let y = 0;
-let [appleX, appleY] = randomCoord();
+class Coord {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+let snakeParts = [new Coord(0, 0), new Coord(20, 0), new Coord(40, 0), new Coord(60, 0)];
+
+let size = 30
+let squares = 15
+
+canvas.height = size * squares;
+canvas.width = size * squares;
+
+let [appleX, appleY] = [0, 90];
+// let [appleX, appleY] = randomCoord();
 
 let lastPress = "ArrowRight";
 
 ctx.fillStyle = 'green';
-ctx.fillRect(x, y, 20, 20);
+ctx.fillRect(snakeParts[0].x, snakeParts[0].y, size, size);
 
 const log = document.getElementById("log");
 
 document.addEventListener('keydown', logKey);
 
-setInterval(step, 500);
+setInterval(step, 250);
 
 function step() {
     // update snake position
+
+    let newSegmentX = snakeParts[0].x;
+    let newSegmentY = snakeParts[0].y;
+
+    for (let i = 0; i < snakeParts.length - 1; i++) {
+        snakeParts[i].x = snakeParts[i + 1].x;
+        snakeParts[i].y = snakeParts[i + 1].y;
+    }
+
+
+    let head = snakeParts[snakeParts.length - 1];
+
     if (lastPress === "ArrowUp") {
-        y -= 20;
+        head.y -= size;
     } else if (lastPress === "ArrowDown") {
-        y += 20;
+        head.y += size;
     } else if (lastPress === "ArrowLeft") {
-        x -= 20;
+        head.x -= size;
     } else if (lastPress === "ArrowRight") {
-        x += 20;
+        head.x += size;
+
     }
 
     // handle apple gobbling and update apple position
-    if (x === appleX && y === appleY) {
+    if (head.x === appleX && head.y === appleY) {
         console.log("gobble!");
         [appleX, appleY] = randomCoord();
+        snakeParts.unshift(new Coord(newSegmentX, newSegmentY));
     }
 
     redraw();
@@ -39,11 +67,15 @@ function step() {
 
 function redraw() {
     ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, 600, 600);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'red';
-    ctx.fillRect(appleX, appleY, 20, 20);
-    ctx.fillStyle = 'green';
-    ctx.fillRect(x, y, 20, 20);
+    ctx.fillRect(appleX, appleY, size, size);
+    snakeParts.forEach(function (segment) {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(segment.x, segment.y, size, size);
+    });
+
+
 }
 
 
@@ -52,8 +84,8 @@ function logKey(e) {
 }
 
 function randomCoord() {
-    let appleX = 20 * Math.floor(Math.random() * 30);
-    let appleY = 20 * Math.floor(Math.random() * 30);
+    let appleX = size * Math.floor(Math.random() * squares);
+    let appleY = size * Math.floor(Math.random() * squares);
     let appleCoords = [appleX, appleY];
     return appleCoords;
 }
